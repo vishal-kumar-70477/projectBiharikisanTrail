f3f0927cca3933efcde2a43eebedb42834059bb3
vishal kumar <vishalkumar704776@gmail.com>
2026-09-02 16:45:28 +0530

    visit

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended:true}));
-app.use(express.json());
+app.use(express.json())
 app.use(cookieParser())
 app.use("/biharikisan/auth",buyerAuthRouter);
 // seller feature routes
